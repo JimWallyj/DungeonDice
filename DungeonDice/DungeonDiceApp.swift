e25196698df16c20cec1e6ce035a6f45b832adb7
Jim Walejko <jwalejko@me.com>
2026-02-17 20:32:11 -0700

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Jim Walejko on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
