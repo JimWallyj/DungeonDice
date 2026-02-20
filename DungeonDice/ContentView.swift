9ab3107245fdcead794fb195a0ef9bf8d3ba1805
Jim Walejko <jwalejko@me.com>
2026-02-19 22:17:06 -0700

    Prof. Gallaugher SwiftUI programming course - Chapter 4.1 Basic version of Dungeon Dice - Dice rolling app 2026.

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -8,15 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "Roll a die!"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dungeon Dice!")
+            
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(.red)
+            Spacer()
+           
+            Text(message)
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+            Spacer()
+            
+            VStack{
+                HStack {
+                    Button("4-sided"){
+                        message = rollDie(sides: 4)
+                    }
+                    Spacer()
+                    Button("6-sided"){
+                        message = rollDie(sides: 6)
+                    }
+                    Spacer()
+                    Button("8-sided"){
+                        message = rollDie(sides: 8)
+                    }
+                }
+                
+                HStack {
+                    Button("10-sided"){
+                        message = rollDie(sides: 10)
+                    }
+                    Spacer()
+                    Button("12-sided"){
+                        message = rollDie(sides: 12)
+                    }
+                    Spacer()
+                    Button("20-sided"){
+                        message = rollDie(sides: 20)
+                    }
+                }
+                
+                Button("100-sided"){
+                    message = rollDie(sides: 100)
+                }
+                
+            }// vStack
+            .buttonStyle(.borderedProminent)
+            .foregroundStyle(.white)
+            .tint(.red)
+            .font(.title2)
         }
         .padding()
     }
+    
+    func rollDie(sides: Int) -> String{
+        return "You rolled \(Int.random(in: 1...sides)) on a \(sides) sided die."
+        // Instructor solution =
+        // let result = Int.random(in: 1...sides)
+        // message = "You rolled a \(result) on a \(sides)-sided die."
+    }
+    
 }
 
 #Preview {
