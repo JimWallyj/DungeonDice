d8fe0e286e63db752baed27e32a66961fc54dc8e
Jim Walejko <jwalejko@me.com>
2026-03-05 21:00:51 -0700

    Prof. John Gallugher  SwiftUI Programming course Chapter 4.x = Custom Dice enum, Type's availability & changes based on where it is defined, using enum cases in strings vs SwiftUI Labels, removing "return" from one-line closures, creating identifable enums.

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum Dice: Int, CaseIterable, Identifiable{
+        case four = 4, six = 6, eight = 8, ten = 10, twelve = 12, twenty = 20, hundred = 100
+        
+        //var dieName: String { "\(self)".capitalized }
+        var id: Int{self.rawValue}
+        
+        var roll: Int{ Int.random(in: 1...self.rawValue)
+        }
+    }
+    
     @State private var message = "Roll a die!"
-    private let diceTypes = [4, 6, 8, 10, 12, 20, 100]
+    //private let diceTypes = [4, 6, 8, 10, 12, 20, 100]
+    var diceTypes: [Dice] = []
     var body: some View {
         VStack {
             Text("Dungeon Dice!")
@@ -22,12 +33,14 @@ struct ContentView: View {
             Text(message)
                 .font(.largeTitle)
                 .multilineTextAlignment(.center)
-            
+        
             Spacer()
             
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 110))]){
-                ForEach(diceTypes, id: \.self) { diceType in Button("\(diceType)-sided"){
-                    rollDie(sides: diceType)
+//                ForEach(Dice.allCases, id: \.self) { die in Button("\(die.rawValue)-sided"){
+                ForEach(Dice.allCases) { die in Button("\(die.rawValue)-sided"){
+                    //rollDie(sides: diceType.rawValue)
+                    message = "You rolled a \(die.roll) on a \(die)-sided die."
                 }
                 .font(.title2)
                 .lineLimit(1)
@@ -40,6 +53,21 @@ struct ContentView: View {
         }
         .padding()
     }
+    
+//    enum Dice: Int{
+//        case four = 4
+//        case six = 6
+//        case eight = 8
+//        case ten = 10
+//        case twelve = 12
+//        case twenty = 20
+//        case hundred = 100
+//    }
+    
+//    func roll(sides: Int){
+//        let result = Int.random(in: 1...sides)
+//        message = "You rolled a \(result) on a \(sides)-sided die."
+//    }
     
     func rollDie(sides: Int) {
         //return "You rolled \(Int.random(in: 1...sides)) on a \(sides) sided die."
