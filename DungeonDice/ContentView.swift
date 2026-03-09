d4fbab7ccdc0bd91758d2e0ad0446f2e3c64d649
Jim Walejko <jwalejko@me.com>
2026-03-08 21:10:29 -0700

    Prof. Gallaugher SwiftUI programminbg course, Chapter 4.7 - Flip a Text View & More, using withAnimation in SwiftUI

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -20,6 +20,9 @@ struct ContentView: View {
     
     @State private var message = "Roll a die!"
     //private let diceTypes = [4, 6, 8, 10, 12, 20, 100]
+    @State private var animationTrigger  = false  // changed when animation should occur
+    @State private var isDoneAnimating = true
+    
     var diceTypes: [Dice] = []
     var body: some View {
         VStack {
@@ -33,12 +36,29 @@ struct ContentView: View {
             Text(message)
                 .font(.largeTitle)
                 .multilineTextAlignment(.center)
-        
+//                .scaleEffect(isDoneAnimating ? 1.0 : 0.6)  //  animate to 1.0
+//                .opacity(isDoneAnimating ? 1.0 : 0.2)
+                .rotation3DEffect(
+                    isDoneAnimating ? .degrees(360) : .degrees(0),
+                                          axis: (1, 0, 0)
+                )
+                //.animation(.default, value: animationTrigger)
+                .onChange(of: animationTrigger){
+                    isDoneAnimating = false  //  set to the beginning "false" state right away
+//                    withAnimation(.spring(response: 0.3, dampingFraction: 0.4)) {
+//                        isDoneAnimating = true
+//                    }
+                    withAnimation(.interpolatingSpring(duration: 0.6, bounce: 0.4)) {
+                        isDoneAnimating = true
+                    }
+                }
+            
             Spacer()
             
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 110))]){
 //                ForEach(Dice.allCases, id: \.self) { die in Button("\(die.rawValue)-sided"){
                 ForEach(Dice.allCases) { die in Button("\(die.rawValue)-sided"){
+                    animationTrigger.toggle()
                     //rollDie(sides: diceType.rawValue)
                     message = "You rolled a \(die.roll) on a \(die)-sided die."
                 }
