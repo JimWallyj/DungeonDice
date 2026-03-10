a1246099bade1ffbf6ad9d34674c5fd3975f1c88
Jim Walejko <jwalejko@me.com>
2026-03-09 21:49:35 -0700

    Prof. John Gallugher SwiftUI programming course Chapter 4.8 - SwiftUI GroupBox and contentTransition numericText for multiple animated Dice rolls. (2026).

--- DungeonDice/ContentView.swift
+++ DungeonDice/ContentView.swift
@@ -9,21 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     enum Dice: Int, CaseIterable, Identifiable{
-        case four = 4, six = 6, eight = 8, ten = 10, twelve = 12, twenty = 20, hundred = 100
-        
-        //var dieName: String { "\(self)".capitalized }
+        case d4 = 4, d6 = 6, d8 = 8, d10 = 10, d12 = 12, d20 = 20, d100 = 100
+    
         var id: Int{self.rawValue}
-        
         var roll: Int{ Int.random(in: 1...self.rawValue)
         }
     }
     
     @State private var message = "Roll a die!"
-    //private let diceTypes = [4, 6, 8, 10, 12, 20, 100]
     @State private var animationTrigger  = false  // changed when animation should occur
     @State private var isDoneAnimating = true
+    @State private var rolls: [Int] = []  //  Empty array of ints for now. We will change it.
+    private var grandTotal: Int { rolls.reduce(0, +)}
     
-    var diceTypes: [Dice] = []
     var body: some View {
         VStack {
             Text("Dungeon Dice!")
@@ -31,23 +29,52 @@ struct ContentView: View {
                 .fontWeight(.black)
                 .foregroundColor(.red)
             
+           
+            GroupBox{
+                ForEach(rolls, id: \.self) { roll in
+                    Text("\(roll)")
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Divider()
+                    
+                }
+                
+                HStack {
+                    Text("TOTAL: \(grandTotal)")
+                        .font(.title2)
+                        .bold()
+                        .monospacedDigit()
+                        .contentTransition(.numericText())
+                        .animation(.default, value: grandTotal)
+                    
+                    Spacer()
+                    
+                    Button("Clear"){
+                        rolls.removeAll()
+                    }
+                    .tint(.red)
+                    .disabled(rolls.isEmpty)
+                }
+                
+                
+                
+            }label: {
+                    Text("Session Rolls:")
+                    .font(.title2)
+                    .bold()
+                }
+            
             Spacer()
            
             Text(message)
-                .font(.largeTitle)
+                .font(.title)
                 .multilineTextAlignment(.center)
-//                .scaleEffect(isDoneAnimating ? 1.0 : 0.6)  //  animate to 1.0
-//                .opacity(isDoneAnimating ? 1.0 : 0.2)
                 .rotation3DEffect(
                     isDoneAnimating ? .degrees(360) : .degrees(0),
                                           axis: (1, 0, 0)
                 )
-                //.animation(.default, value: animationTrigger)
                 .onChange(of: animationTrigger){
                     isDoneAnimating = false  //  set to the beginning "false" state right away
-//                    withAnimation(.spring(response: 0.3, dampingFraction: 0.4)) {
-//                        isDoneAnimating = true
-//                    }
                     withAnimation(.interpolatingSpring(duration: 0.6, bounce: 0.4)) {
                         isDoneAnimating = true
                     }
@@ -56,11 +83,11 @@ struct ContentView: View {
             Spacer()
             
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 110))]){
-//                ForEach(Dice.allCases, id: \.self) { die in Button("\(die.rawValue)-sided"){
                 ForEach(Dice.allCases) { die in Button("\(die.rawValue)-sided"){
                     animationTrigger.toggle()
-                    //rollDie(sides: diceType.rawValue)
-                    message = "You rolled a \(die.roll) on a \(die)-sided die."
+                    let roll = die.roll
+                    message = "You rolled a \(roll) on a \(die)."
+                    rolls.append(roll)
                 }
                 .font(.title2)
                 .lineLimit(1)
@@ -73,21 +100,6 @@ struct ContentView: View {
         }
         .padding()
     }
-    
-//    enum Dice: Int{
-//        case four = 4
-//        case six = 6
-//        case eight = 8
-//        case ten = 10
-//        case twelve = 12
-//        case twenty = 20
-//        case hundred = 100
-//    }
-    
-//    func roll(sides: Int){
-//        let result = Int.random(in: 1...sides)
-//        message = "You rolled a \(result) on a \(sides)-sided die."
-//    }
     
     func rollDie(sides: Int) {
         //return "You rolled \(Int.random(in: 1...sides)) on a \(sides) sided die."
